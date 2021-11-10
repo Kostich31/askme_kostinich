4e6e452b821a6b857b2eb85fadb2004a3734589d
Kostich31 <k.v.kostinich@mail.ru>
2021-11-10 20:50:46 +0300

    add dz1

---
+++ static/js/like.js
@@ -0,0 +1,21 @@
+// Выбираем все элементы с классом like
+const likes = document.querySelectorAll('.like');
+
+// В каждом элементе выбираем плюс и минус. Навешиваем на событие клик функцию render()
+likes.forEach(like => {
+  const plus = like.querySelector('.plus_btn');
+  const minus = like.querySelector('.minus_btn');
+  const counter_element = like.querySelector('.counter');
+  
+  let counter = 0;
+  plus.addEventListener('click', () => {
+    render(++counter, counter_element);
+  });
+  
+  minus.addEventListener('click', () => {
+    render(--counter, counter_element)
+  });
+});
+
+// Функция обновляет текст
+const render = (counter, counter_element) => counter_element.innerText = counter;
