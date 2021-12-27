b454e24f935ec834f2566a454f82e7c5e23ebff6
Kostich31 <k.v.kostinich@mail.ru>
2021-12-27 22:58:20 +0300

    add dz4
    add dz4

--- app/views.py
+++ app/views.py
@@ -1,22 +1,12 @@
 import random
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
-from random import sample, choice
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
 from app.models import *
-
-
-
-answers = [
-    {
-        'id': idx_ans,
-        'title': f'Answer number {idx_ans}',
-        'text': f'Some text for question #{idx_ans}'
-    } for idx_ans in range(5)
-]
-
-list_all_tags = ['Perl', 'Python', 'TechnoPark', 'MYSQL', 'django', 'Mail.ru', 'Shuvaeva', 'Firefox']
-
+from app.forms import *
+from django.contrib import auth
 
 def paginate(objects_list, request, per_page=5):
     paginator = Paginator(objects_list, per_page)
@@ -27,7 +17,7 @@ def paginate(objects_list, request, per_page=5):
 def index(request):
     question = paginate(Question.objects.newest(), request)
     tag = Tag.objects.all()
-    return render(request, 'index.html', {'questions': question, 'tags': tag})
+    return render(request, "index.html", {"question": question})
 
 
 def hot(request):
@@ -36,32 +26,111 @@ def hot(request):
     return render(request, 'index.html', {'questions': question, 'tags': tag})
 
 
+@login_required(login_url='/login/')
 def ask(request):
-    return render(request, 'ask.html', {})
+    if request.method == "GET":
+        form = AskForm()
+
+    if request.method == "POST":
+        form = AskForm(data=request.POST)
+        if form.is_valid():
+            tags = form.save()
+            user = User.objects.filter(user=request.user).values("id")
+            question = Question.objects.create(author_id=user,
+                                               title=form.cleaned_data["title"],
+                                               text=form.cleaned_data["text"],
+                                               date=datetime.today())
+            for _tag in tags:
+                question.tags.add(_tag)
+                question.save()
+            return redirect("question", pk=question.id)
+    return render(request, "ask.html", {"form": form})
 
 
 def login(request):
-    return render(request, 'login.html', {})
+    redirect_to = request.GET.get('next', '/')
+    error_message = None
+    if request.method == 'GET':
+        form = LoginForm()
+    else:
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            user = auth.authenticate(request, **form.cleaned_data)
+            if user is not None:
+                auth.login(request, user)
+                return redirect(redirect_to)
+            else:
+                error_message = "Sorry, wrong login or password"
+    return render(request, 'login.html', {'form': form, 'redirect_to': redirect_to, 'error_message': error_message})
 
 
 def question(request, pk):
-     question = Question.objects.by_id(pk).first()
-     tag = Tag.objects.all()[0:2]
-     answers = question.answers.all()
-     content = paginate(answers, request,5)
-     return render(request, "question.html", {"question": question, "answers": content,
-                                              "tags": tag})
+    selected_question = Question.objects.by_id(pk).first()
+    selected_answers = Answer.objects.filter(what_question=selected_question)
+    content = paginate(selected_answers, request, 3)
+
+    if request.method == "GET":
+        form = AnswerForm()
+
+    if request.method == "POST":
+        form = AnswerForm(data=request.POST)
+        user = User.objects.filter(user=request.user).values("id")
+        if form.is_valid():
+            answer = Answer.objects.create(question_id=selected_question.id,
+                                           author_id=user,
+                                           text=form.cleaned_data["text"])
+            return redirect(reverse("question", kwargs={"pk": selected_question.id}) + "?page="
+                            + str(content.paginator.num_pages+1))
+
+    return render(request, "question.html",
+                  {"question": selected_question, "content": content, "form": form})
 
 
 def signup(request):
-    return render(request, 'signup.html', {})
+    if request.method == "GET":
+        form = SignUpForm()
+
+    if request.method == "POST":
+        form = SignUpForm(data=request.POST)
+        if form.is_valid():
+            user = form.save()
+            if user is not None:
+                user.set_password(request.POST['password'])
+                user.save()
+                auth.login(request, user)
+                return redirect("index")
+    return render(request, "signup.html", {"form": form})
 
 
+@login_required(login_url='/login/')
 def settings(request):
-    return render(request, 'settings.html', {})
+    if request.method == "GET":
+        user = request.user
+        form = SettingsForm()
+        if not user.is_authenticated:
+            return HttpResponseForbidden()
+
+    if request.method == "POST":
+        form = SettingsForm(data=request.POST)
+        if form.is_valid():
+            user = request.user
+            if user.is_authenticated:
+                if form.cleaned_data["username"] != user.username and form.cleaned_data["username"] != "":
+                    user.username = form.cleaned_data["username"]
+                    user.save()
+                    auth.login(request, user)
+                if form.cleaned_data["email"] != user.email and form.cleaned_data["email"] != "":
+                    user.email = form.cleaned_data["email"]
+                    user.save()
+                    auth.login(request, user)
+    return render(request, "settings.html", {"form": form})
 
 
 def tag(request, tag):
     question = paginate(Tag.objects.question_by_tag(tag), request)
     return render(request, 'tag.html', {'questions': question, "tag": tag})
+
+def logout(request):
+    auth.logout(request)
+    return redirect(request.GET.get('next', '/'))
     
